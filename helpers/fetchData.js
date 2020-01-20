f0c7b077e8203cc6abcac3f335fcb48a35803a49
LeoCantThinkofAName <i7851@hotmail.com>
2020-01-20 10:48:46 +0800

    refactor fetch function

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -2,32 +2,48 @@ import currentProcessor from "./currentProcessor";
 import forecastProcessor from "./forecastProcessor";
 
 const key = "e0e6bc3e4f76bd0cee424878945e0461";
+const currentWeatherApi = "http://api.openweathermap.org/data/2.5/weather";
+const forecastApi = "http://api.openweathermap.org/data/2.5/forecast";
 
-// api example
-// http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}
+// const fetchData = async ({ url, options = {} }) => {
+//   try {
+//     const api = `${url}&APPID=${key}`;
+//     console.log(api);
+//     const res = await fetch(api, options);
+//     const json = await res.json();
+//     const currentProcessed = await currentProcessor(json);
 
-const fetchData = async ({ url, options = {} }) => {
+//     const forecastRes = await fetch(
+//       `http://api.openweathermap.org/data/2.5/forecast?id=${currentProcessed.id}&APPID=${key}`
+//     );
+//     const forecastJson = await forecastRes.json();
+//     const forecastProcessed = await forecastProcessor(forecastJson);
+
+//     return {
+//       ...currentProcessed,
+//       forecast: forecastProcessed,
+//     };
+//   } catch (err) {
+//     console.log(err);
+//     return err;
+//   }
+// };
+
+const fetchData = (params = {}, currentWeather) => {
+  const paramsStringified = Object.entries(params).map(entry => entry.join("=")).join("&");
+
   try {
-    const api = `${url}&APPID=${key}`;
-    console.log(api);
-    const res = await fetch(api, options);
+    const url = currentWeather ? `${forecastApi}?${paramsStringified}&APPID=${key}` : `${currentWeatherApi}?${paramsStringified}&APPID=${key}`
+    const res = await fetch(url);
     const json = await res.json();
-    const currentProcessed = await currentProcessor(json);
+    const processed = currentWeather ? forecastProcessor(json) : currentProcessor(json);
 
-    const forecastRes = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?id=${currentProcessed.id}&APPID=${key}`
-    );
-    const forecastJson = await forecastRes.json();
-    const forecastProcessed = await forecastProcessor(forecastJson);
-
-    return {
-      ...currentProcessed,
-      forecast: forecastProcessed,
-    };
-  } catch (err) {
-    console.log(err);
-    return err;
+    if(currentProcessor) return {currentWeather, forecast: processed};
+    fetching({id: processed.id}, processed);
+  } catch(error) {
+    console.log(error);
+    return error;
   }
-};
+}
 
 export default fetchData;
