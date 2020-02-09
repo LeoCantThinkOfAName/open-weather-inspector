b7ac8acefd2112215aa67d29dd8642381eacfa7b
leocantthinkofanamae <i7851@hotmail.com>
2020-02-10 01:09:30 +0800

    modify dark theme color

--- reducers/theme/themeReducer.js
+++ reducers/theme/themeReducer.js
@@ -3,7 +3,7 @@ import { TOGGLE_THEME } from "./themeType";
 const initialState = {
   theme: true,
   black: "#fff",
-  white: "#333",
+  white: "#222",
 };
 
 export const toggleTheme = (state = initialState, action) => {
@@ -11,8 +11,8 @@ export const toggleTheme = (state = initialState, action) => {
     case TOGGLE_THEME:
       return {
         theme: action.payload,
-        black: action.payload ? "#fff" : "#333",
-        white: action.payload ? "#333333" : "#fff",
+        black: action.payload ? "#fff" : "#222",
+        white: action.payload ? "#222" : "#fff",
       };
     default:
       return state;
